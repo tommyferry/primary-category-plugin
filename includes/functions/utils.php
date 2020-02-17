45489598cfcfa9a6ae3c40012e22991a9b680b6c
Tommy Ferry <tommyferry6@gmail.com>
2020-02-17 21:23:56 +0000

    feat: add filter to primary taxonomy util function

--- includes/functions/utils.php
+++ includes/functions/utils.php
@@ -17,5 +17,16 @@ function get_primary_term_taxonomies() {
 		'public' => true,
 	);
 
-	return get_taxonomies( $args );
+	$taxonomies = get_taxonomies( $args );
+
+	/**
+	 * Filter the list of taxonomies
+	 *
+	 * @hook pcp_primary_term_taxonomies
+	 *
+	 * @param {string[]} An array of taxonomy names that will be given a primary term.
+	 *
+	 * @return {string[]} The filtered list of taxonomy names.
+	 */
+	return apply_filters( 'pcp_primary_term_taxonomies', $taxonomies );
 }
