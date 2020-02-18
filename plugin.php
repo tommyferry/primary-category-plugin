6548a7dfdfe37ab4e0a0655b079399da88dfb5ac
Tommy Ferry <tommyferry6@gmail.com>
2020-02-18 00:19:44 +0000

    docs: update plugin version number

--- plugin.php
+++ plugin.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Plugin Name: Primary Category Plugin
- * Plugin URI: https://example.com
+ * Plugin URI:  https://example.com
  * Description: Adds the ability to set a primary category for posts.
- * Version:     0.1.0
+ * Version:     1.0.0
  * Author:      Tommy Ferry
  * Author URI:  https://example.com
  * Text Domain: primary-category-plugin
@@ -13,7 +13,7 @@
  */
 
 // Useful global constants.
-define( 'PRIMARY_CATEGORY_PLUGIN_VERSION', '0.1.0' );
+define( 'PRIMARY_CATEGORY_PLUGIN_VERSION', '1.0.0' );
 define( 'PRIMARY_CATEGORY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'PRIMARY_CATEGORY_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'PRIMARY_CATEGORY_PLUGIN_INC', PRIMARY_CATEGORY_PLUGIN_PATH . 'includes/' );
